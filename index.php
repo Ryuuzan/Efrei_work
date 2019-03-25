60b8de80e5bb7c91a1992c7982667b0d8cb6685d
Alexandre <alexmilo96@wanadoo.fr>
2019-03-25 08:53:01 +0100

    Friendbook online

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
-<?php
-$names = array("Patrick", "John", "Marie", "Claire", "Paul");
-foreach ($names as $name) {
-   	print "$name, ";
+<form action="index.php" method="post">
+Name: <input type="text" name="name">
+<input type="submit">
+</form>
+<ul>
+<?php $filename = 'friends.txt';
+$file = fopen( $filename, "r" );
+while (!feof($file)&&filesize('friends.txt') != 0) {
+    echo "<li>".fgets($file)."</li>";
 }
 ?>
+</ul>
