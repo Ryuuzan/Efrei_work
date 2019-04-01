d26c96789feda4023aeac70fbf26753c3509e32f
Alexandre <alexmilo96@wanadoo.fr>
2019-04-01 18:35:45 +0200

    Friendbook Advance Filter Function

--- index.php
+++ index.php
@@ -13,21 +13,47 @@ if(isset($_POST["name"]))
 	Name: <input type="text" name="name">
 	<input type="submit">
 </form>
+
 <?php
 $file = fopen( $filename, "r" );
-while (!feof($file)) {
-	$word=fgets($file);
-	if(isset($_GET["nameFilter"])){
-		if(strstr($word,$_GET["nameFilter"]))
-			{echo "<li>".$word."</li>";}
-	}
-	else{
-		echo "<li>".$word."</li>";}
-	}
-	fclose($file);
-	?>
 
-	<form action="index.php" method="get">
-		Filter: <input type="text" name="nameFilter">
-		<input type="submit">
-	</form>
+while (!feof($file)) {
+
+	$word=fgets($file);
+
+
+	if(isset($_GET["nameFilter"]))
+	{
+
+		if (isset($_GET["startingWith"]))
+		{
+			$pos = strpos(strtolower ($word), strtolower ($_GET["nameFilter"]));
+
+			if ($pos !== false) 
+			{
+				if($pos===0)
+				echo "<li>".$word."</li>";
+			}
+			
+
+		}
+		else
+		{
+			if(strstr(strtolower ($word),strtolower ($_GET["nameFilter"])))
+				{echo "<li>".$word."</li>";}
+		}
+
+	}
+	else
+	{
+		echo "<li>".$word."</li>";
+	}
+}
+fclose($file);
+?>
+
+<form action="index.php" method="get">
+	Filter: <input type="text" name="nameFilter">
+	<input type="checkbox" name="startingWith">Only names starting with</input>
+	<input type="submit">
+</form>
